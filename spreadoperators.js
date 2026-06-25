c2e4e3c1c358fe6adaaaba1401d7700370f67b8d
ranjithkumarsunnam <sunnamranjith7@gmail.com>
2026-06-25 22:51:58 +0530

    practice spreadOperators in javascript

--- spreadoperators.js
+++ spreadoperators.js
@@ -23,7 +23,7 @@ let fullstack=[...frontend,...backend];
 console.log(fullstack);
 
 
-//spreadoperators
+//spreadoperators adding new data
 
 let numbers =[1,2,3,4];
 let updatedNumbers=[...numbers,5,6];
@@ -73,3 +73,109 @@ console.log(user);
 let maximum =[10,80,100,60];
 let max=Math.max(...maximum);
 console.log(max);
+
+
+
+//Adding new User (API)
+
+const Users =[
+    {id:1, name:"ranjith"},
+    {id:2 ,name:"rakesh"}
+];
+const newUser={id:3,name:"saiTeja"};
+
+const UpdatedUsers={...Users,newUser};
+console.log(UpdatedUsers);
+
+
+
+//Updating UserProfile
+
+const user1={
+    name:"ramu",
+    age:20,
+    role:"Developer",
+    city:"HYD"
+}
+
+const updateUser={
+    ...user1,
+    city:"VIZ"
+};
+
+console.log(updateUser)
+
+
+
+//Merge Requested Data
+
+const dbData={
+    id:1,
+    name:"Sai"
+}
+
+const requestedData={
+    city:"Hyderabad",
+    age:25
+}
+
+const UpdatedData ={
+    ...dbData,...requestedData
+};//Used in API processing Requests
+
+console.log(UpdatedData);
+
+
+//Sending API Response
+
+const user2={
+    id:1,
+    name:"Ranjith",
+};
+
+const response={
+    success:true,
+    ...user2
+};
+console.log(response);
+
+//Combining the Products In DataBase
+
+const mobiles=["iphone","Samsung"];
+const laptops=["Dell, HP"];
+
+const products =[...mobiles,...laptops];
+console.log(products);
+
+
+//Loggin Request Information
+
+/* const request={
+    userId:"ranjith",
+    action:"LOGIN"
+};
+const log={
+    timestamp:Data.now(), ...request
+}
+console.log(log); */
+
+
+
+//DataBase Example 
+
+const userInfo={
+    _id:1,
+    name:"Ramudu",
+    age:"undifined",
+    job:"protect"
+}
+//Received from frontend
+
+const ReceivedInfo={
+    job:" Save the nature"
+}
+const UpdatedUsers1={
+    ...userInfo,...ReceivedInfo
+};
+
+console.log(UpdatedUsers1);
