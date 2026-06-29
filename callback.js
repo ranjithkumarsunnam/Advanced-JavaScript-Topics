25905fe4d74f4ff2d4d76cca65df19896bfd6bca
ranjithkumarsunnam <sunnamranjith7@gmail.com>
2026-06-29 23:44:08 +0530

    callback function setting time to function using callback

--- callback.js
+++ callback.js
@@ -1,4 +1,4 @@
-function greet(name,callback){
+/* function greet(name,callback){
     console.log("hello");
     callback();
 }
@@ -51,3 +51,147 @@ function Gree(callback){
 Gree(()=>{
     console.log("Good Morning");
 });
+
+function sendEmail(email,callback){
+    console.log(`Sending email to ${email}`);
+    callback();
+}
+function emailsent(){
+    console.log("Email sent successfull!");
+}
+sendEmail("ranjithkumar@gmail",emailsent);
+
+
+//online food Order
+
+function placeOrder(item,callback){
+    console.log(`${item} order Successfully ` );
+    callback();
+}
+function preparingOrder(){
+    console.log("Preparing Food...");
+}
+placeOrder("pizza",preparingOrder);
+
+
+//ATM Withdrawal
+
+function withDraw(amount,callback){
+    console.log(`Withdrawing amount ${amount}`);
+    callback();
+}
+function printRecipt(){
+    console.log("Printing receipt...");
+}
+withDraw(5000,printRecipt);
+
+//Shopping App
+
+function addCart(product,callback){
+    console.log(`${product} added to cart`);
+    callback();
+}
+function checkout(){
+    console.log(`Proceeding to checkout....`);
+}
+addCart("laptop",checkout);
+
+
+//Booking Ticket 
+
+function bookingTicket(movie,callback){
+    console.log(`${movie} ticket is Booked`);
+    callback();
+}
+function Review(){
+    console.log("Movie is Blockblaster......");
+}
+bookingTicket("RRR",Review);
+
+
+//currentBill
+function payBill(amount,callback){
+    console.log(`This mount currentBill ${amount}`);
+    callback();
+}
+function receipt(){
+    console.log("receipt is generated...")
+}
+payBill(500,receipt); */
+
+
+
+
+
+
+
+//Student Details 
+function checkResult(name ,callback){
+    console.log(`${name} 's result is ready`);
+    callback()
+}
+function showGrade(){
+    console.log(`Grade A`);
+}
+checkResult("ranjith",showGrade);
+
+
+//Shopping App
+
+function addCart(product,callback){
+    console.log(`${product} is Added to cart`);
+    callback();
+}
+function checkout(){
+    console.log(`Proceeding to checkout`);
+}
+addCart(`laptop`,checkout);
+
+
+function sendOTP(callback){
+    console.log(`Sending OPT`);
+    callback();
+}
+setTimeout(()=>{
+    console.log(`OPT Sent`);
+    callback();
+},2000);
+
+function verifyOTP(){
+    console.log("Please verify your OTP");
+}
+sendOTP(verifyOTP);
+
+//Download File
+
+function downloadFiles(callback){
+    console.log("Download file....")
+    setTimeout(()=>{
+        console.log("download Complated");
+    },3000)
+}
+function openfile(){
+    console.log(`Opening File.......`);
+}
+downloadFiles(openfile)
+
+
+
+
+
+//Database User
+function getUser(callback){
+    console.log(`Feching user.....`);
+    setTimeout(()=>{
+        console.log("User Found ");
+        callback();
+    },2000)
+}
+function displayUser(){
+    console.log("displaying user Details");
+}
+getUser(displayUser);
+
+
+
+
